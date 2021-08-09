94ec32c5d047e57e0acf87ae2fa34fc56fbd0699
thiagoribb <thiribeiro142@gmail.com>
2021-08-09 16:46:34 -0300

    Preparando revisão de código

--- src/services/professors.ts
+++
@@ -1,8 +0,0 @@
-import { Professors } from 'src/entities/Professors';
-import { getRepository } from 'typeorm';
-
-const repository = getRepository(Professors);
-
-export const find = async (): Promise<Professors[]> => {
-  return repository.find();
-};
